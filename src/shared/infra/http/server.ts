34c36fcff094cc4a0c26b1274818add7fec51063
Matheus <matheusfrancoalves@hotmail.com>
2021-03-17 18:50:16 -0300

    :sparkles: create user router

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
+import "reflect-metadata"
+
 import express from 'express';
 import cors from 'cors';
 
+import routes from './routes';
 import '../typeorm';
 import '../../container';
 
@@ -8,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes)
 
 app.listen(3333, () => {
     console.log('Server is running on port 3333')
