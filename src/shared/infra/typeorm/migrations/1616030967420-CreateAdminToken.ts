0d376152dd927cc3c426ffb46dbcc0c8083a20a3
Matheus <matheusfrancoalves@hotmail.com>
2021-03-17 22:31:14 -0300

    :sparkles: create admin token migration

---
+++ src/shared/infra/typeorm/migrations/1616030967420-CreateAdminToken.ts
@@ -0,0 +1,55 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateAdminToken1616030967420 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'admin_token',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'token',
+                        type: 'uuid',
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'admin_id',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: 'AdminToken',
+                        referencedTableName: 'admins',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['admin_id'],
+                        onDelete: 'CASCADE',
+                        onUpdate: 'CASCADE',
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
