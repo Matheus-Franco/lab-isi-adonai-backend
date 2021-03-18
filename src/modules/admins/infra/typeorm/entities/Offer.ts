ae2b5dc5349f422ff3aae1679713da55bffdbe20
Matheus <matheusfrancoalves@hotmail.com>
2021-03-18 19:39:41 -0300

    :sparkles: create offers migration and entity

---
+++ src/modules/admins/infra/typeorm/entities/Offer.ts
@@ -0,0 +1,33 @@
+import { 
+    Column, 
+    CreateDateColumn, 
+    Entity, 
+    PrimaryGeneratedColumn,
+    UpdateDateColumn 
+} from 'typeorm';
+
+@Entity('offers')
+class Admins {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: string;
+
+    @Column()
+    year_model: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Admins;
