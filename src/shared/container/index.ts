0ddd3cd6d308f9fc94a4fe370f0fa1496b148fb1
Matheus <matheusfrancoalves@hotmail.com>
2021-03-18 17:09:36 -0300

    :sparkles: add admins dependency to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,5 +6,14 @@ import ClientsRepository from '../../modules/clients/infra/typeorm/repositories/
 import IClientTokenRepository from '../../modules/clients/repositories/IClientTokenRepository';
 import ClientTokenRepository from '../../modules/clients/infra/typeorm/repositories/ClientTokenRepository';
 
+import IAdminsRepository from '../../modules/admins/repositories/IAdminsRepository';
+import AdminsRepository from '../../modules/admins/infra/typeorm/repositories/AdminsRepository';
+
+import IAdminTokenRepository from '../../modules/admins/repositories/IAdminTokenRepository';
+import AdminTokenRepository from '../../modules/admins/infra/typeorm/repositories/AdminTokenRepository';
+
 container.registerSingleton<IClientsRepository>('ClientsRepository', ClientsRepository);
 container.registerSingleton<IClientTokenRepository>('ClientTokenRepository', ClientTokenRepository);
+
+container.registerSingleton<IAdminsRepository>('AdminsRepository', AdminsRepository);
+container.registerSingleton<IAdminTokenRepository>('AdminTokenRepository', AdminTokenRepository);
