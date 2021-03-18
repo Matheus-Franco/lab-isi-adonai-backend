15ee0b68c3998b761a7496b39ee933e77c1bbc22
Matheus <matheusfrancoalves@hotmail.com>
2021-03-18 17:02:15 -0300

    :sparkles: create authenticate admin service

---
+++ src/modules/admins/services/AuthenticateAdminService.ts
@@ -0,0 +1,53 @@
+import "reflect-metadata"
+
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe'
+import { compare } from "bcryptjs";
+
+import authenticationConfig from '../../../config/authenticationConfig';
+
+import Admins from "../infra/typeorm/entities/Admin";
+import IAdminsRepository from "../repositories/IAdminsRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    admin: Admins;
+    token: string;
+}
+
+@injectable()
+class AuthenticateAdminService {
+    constructor(
+        @inject('AdminsRepository')
+        private adminsRepository: IAdminsRepository,
+    ) { }
+
+    public async execute({ email, password }: IRequest): Promise<IResponse> {
+        const admin = await this.adminsRepository.findByEmail(email);
+
+        if (!admin) {
+            throw new Error('E-mail ou senha incorretos');
+        }
+
+        const passwordMatched = await compare(password, admin.password);
+
+        if (!passwordMatched) {
+            throw new Error('E-mail ou senha incorretos.');
+        }
+
+        const { secret, expiresIn } = authenticationConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: admin.id,
+            expiresIn,
+        });
+
+        return { admin, token };
+    }
+}
+
+export default AuthenticateAdminService;
