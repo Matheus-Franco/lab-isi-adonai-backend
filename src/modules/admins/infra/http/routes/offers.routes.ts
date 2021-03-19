5cb099badc17933592ffcce6ad37738f30131d36
Matheus <matheusfrancoalves@hotmail.com>
2021-03-18 23:13:22 -0300

    :sparkles: use middleware

--- src/modules/admins/infra/http/routes/offers.routes.ts
+++ src/modules/admins/infra/http/routes/offers.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
 import OffersController from '../controllers/OffersController'
+import ensureAuthenticated from '../../../../../shared/infra/http/middlewares/ensureAuth';
 
 const offersRouter = Router()
 const offersController = new OffersController();
 
-offersRouter.post('/', offersController.create)
+offersRouter.post('/', ensureAuthenticated , offersController.create)
 offersRouter.get('/', offersController.index)
 
 export default offersRouter
